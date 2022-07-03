faad688323f545408c1f515da4f874872ffeb028
SushiTuna <cjlubosana@gmail.com>
2022-07-03 09:40:46 +0800

    Add DaisyUI and build pipeline

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.static('dist'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Server is running on: http://localhost:${port}`));
